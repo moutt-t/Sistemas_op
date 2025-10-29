8b7ff95f56de6778521b51167694eef304951fa8
moutt-t <juanpablomottatalero@gmail.com>
2025-10-29 09:51:09 -0500

    Update lab_01.c

--- Laboratorio Posix Concurrencia/lab_01.c
+++ Laboratorio Posix Concurrencia/lab_01.c
@@ -1,31 +1,63 @@
 /**************************************************************
-*Fecha: 28/10/2025
-*Autor: Juan Pablo motta talero 
-*Materia: Sistemas operativos
-*Tema: Posix, implementacion de pthreads
+* UNIVERSIDAD: Pontificia Universidad Javeriana
+* PROGRAMA: Ingeniería de Sistemas
+* MATERIA: Sistemas Operativos
+* TEMA: POSIX Threads (pthreads) - Implementación básica
+* FECHA: 28/10/2025
+* AUTOR: Juan Pablo Motta Talero
+* DESCRIPCIÓN:
+* Este programa demuestra el uso de hilos en C utilizando la
+* biblioteca POSIX Threads (pthread). Se crean dos hilos
+* independientes que ejecutan una función común encargada de
+* imprimir un mensaje en pantalla. 
+* OBJETIVO:
+* - Comprender la creación, ejecución y finalización de hilos
+*   (threads) en entornos POSIX.
+* - Observar cómo múltiples hilos pueden ejecutarse de forma
+*   concurrente dentro de un mismo proceso.
 *************************************************************/
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-
-void *print_message_function( void *ptr ) {
+#include <stdio.h>  
+#include <stdlib.h>    
+#include <pthread.h>    
+/**************************************************************
+* FUNCIÓN: print_message_function
+* PARÁMETROS:
+*   - void *ptr : puntero genérico que recibirá el mensaje
+*                 (convertido a tipo char*)
+**************************************************************/
+void *print_message_function(void *ptr) {
     char *message;
-    message = (char *) ptr;
-    printf("%s \n", message);
+    message = (char *) ptr;    // Conversión del puntero genérico
+    printf("%s \n", message);  // Imprime el mensaje del hilo
+    pthread_exit(NULL);        // Finaliza el hilo de forma segura
 }
 
-void main() {
-    pthread_t thread1, thread2;
-    char *message1 = "Thread 1";
-    char *message2 = "Thread 2";
+/**************************************************************
+* DESCRIPCIÓN:
+*   Crea dos hilos independientes que ejecutan la misma función
+*   (print_message_function), pero con diferentes mensajes.
+*   Posteriormente imprime los códigos de retorno de la creación
+*   de cada hilo.
+*
+**************************************************************/
+int main() {
+    pthread_t thread1, thread2;     // Identificadores de los hilos
+    char *message1 = "Mensaje del hilo 1";
+    char *message2 = "Mensaje del hilo 2";
     int iret1, iret2;
 
-    /* Create independent threads each of which will execute function */
-    iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
-    iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
+    /* Creación de los hilos */
+    iret1 = pthread_create(&thread1, NULL, print_message_function, (void*) message1);
+    iret2 = pthread_create(&thread2, NULL, print_message_function, (void*) message2);
 
-    printf("Thread 1 returns: %d\n", iret1);
-    printf("Thread 2 returns: %d\n", iret2);
-    exit(0);
+    /* Verificación del resultado de la creación */
+    printf("Creación del hilo 1, código de retorno: %d\n", iret1);
+    printf("Creación del hilo 2, código de retorno: %d\n", iret2);
+
+    /* Esperar la finalización de ambos hilos */
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Ejecución de ambos hilos finalizada.\n");
+    return 0;
 }
-
