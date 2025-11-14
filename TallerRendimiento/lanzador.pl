24240d2ab3a7dd2a5be973dbf94372d45b1dc2d6
moutt-t <juanpablomottatalero@gmail.com>
2025-11-14 18:39:42 -0500

    Add files via upload

---
+++ TallerRendimiento/lanzador.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+#**************************************************************
+#         		Pontificia Universidad Javeriana
+#     Autor: Carolina Ujueta Ricardo
+#     Fecha: 13 de noviembre, 2025
+#     Materia: Sistemas Operativos
+#     Tema: Taller de Evaluación de Rendimiento
+#     Fichero: script automatización ejecución por lotes 
+#****************************************************************/
+
+$Path = `pwd`;
+chomp($Path);
+
+$Nombre_Ejecutable = "MM_ejecutable";
+@Size_Matriz = ("2","10","20");
+@Num_Hilos = (1,2,4,8,16,20);
+$Repeticiones = 4;
+
+foreach $size (@Size_Matriz){
+	foreach $hilo (@Num_Hilos) {
+		$file = "$Path/$Nombre_Ejecutable-".$size."-Hilos-".$hilo.".dat";
+		for ($i=0; $i<$Repeticiones; $i++) {
+#system("$Path/$Nombre_Ejecutable $size $hilo  >> $file");
+			printf("$Path/$Nombre_Ejecutable $size $hilo \n");
+		}
+		close($file);
+	$p=$p+1;
+	}
+}
